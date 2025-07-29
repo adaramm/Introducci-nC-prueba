24f191e9108b8c3a0afa863a5387c04c6a4f4d0a
adaramm <ariel.mercado@ingenieria.unam.edu>
2025-07-29 12:54:00 -0600

    Update test_solution.cpp

--- test/test_solution.cpp
+++ test/test_solution.cpp
@@ -1,1 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../include/solution.hpp"
 
+TEST_CASE("Suma de pares") {
+    REQUIRE(sum_even({1, 2, 3, 4, 5, 6}) == 12);
+    REQUIRE(sum_even({1, 3, 5}) == 0);
+    REQUIRE(sum_even({2, -2, 4}) == 4);
+    REQUIRE(sum_even({}) == 0);
+}
